afcfb34763af3f6daa431d0be71f1d4dc213b685
Martin <martinconradpelayo@gmail.com>
2017-02-06 18:29:07 -0800

    week1-lab1

--- app.js
+++ app.js
@@ -1,1 +1,18 @@
-console.log('What up');
+'use strict';
+
+var question1 = prompt('What is your name?');
+    console.log('User answer for question 1: ' + question1);
+    alert('Sup ' + question1 + ', nice to meet yah');
+
+var question2 = prompt('Where are you from?');
+    console.log('Answer to question 2: ' + question2);
+    alert(question2 + ', That is a nice place ' + question1);
+
+var question3 = prompt('What is your favorite color?');
+    console.log('Answer to question3: ' + question3);
+    alert(question1 + ' from ' + question2 + ', ' + question3 + '!, THATS GRRRREAT!');
+
+var question4 = prompt('Is it raining outside?');
+    console.log('Answer to question4: ' + question4 );
+    alert(question4 + ', YES! ' + question1 + ' from ' + question2 + ', and did I mention ' + question3 + ' I a great color?');
+
